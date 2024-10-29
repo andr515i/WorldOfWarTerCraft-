f4fce4124a3f24df910890f7f49280c4ced9bc1e
L-tech-s <joachimlow02@gmail.com>
2024-10-29 13:48:16 +0100

    Update CommandGo.cs

--- CommandGo.cs
+++ CommandGo.cs
@@ -8,7 +8,7 @@ class CommandGo : BaseCommand, ICommand {
   
   public void Execute (Context context, string command, string[] parameters) {
     if (GuardEq(parameters, 1)) {
-      Console.WriteLine("I don't seem to know where that is 🤔");
+      Console.WriteLine("Room doesn't exist");
       return;
     }
     context.Transition(parameters[0]);

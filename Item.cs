12f9828fe34b164e6f4b5c9c0c27fe8e7b166ca8
L-tech-s <joachimlow02@gmail.com>
2024-11-03 15:15:03 +0100

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -14,35 +14,6 @@ public Item (string name, int effect, int keyNumber)
     this.name = name; 
     this.effect = effect; 
     this.keyNumber = keyNumber; 
-    { 
-    case 1:
-        itemDescription = "asdasdaf";
-        break;
-    case 2:
-        itemDescription = "asdasdaf";
-        break;        
-    case 3:
-        itemDescription = "asdasdaf";
-        break;
-    case 4:
-        itemDescription = "asdasdaf";
-        break;
-    case 5:
-        itemDescription = "asdasdaf";
-        break;
-    case 6:
-        itemDescription = "asdasdaf";
-        break;
-    case 7:
-        itemDescription = "asdasdaf";
-        break;
-    case 8:
-        itemDescription = "asdasdaf";
-        break;
-    case 9:
-        itemDescription = "asdasdaf";
-        break;
-    }
 }
 // methods 
 public string GetName() 

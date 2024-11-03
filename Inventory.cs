ab678ad6d75b749a9fd29acfd1d7ba90e52dc0fa
L-tech-s <joachimlow02@gmail.com>
2024-11-03 16:46:43 +0100

    Update Inventory.cs

--- Inventory.cs
+++ Inventory.cs
@@ -1,9 +1,11 @@
-/*class Inventory 
+using System.Security.Cryptography;
+
+class Inventory 
 {
 // Atributes 
-    private Items[] Items;
+    private Item[] Items;
 // Constructor 
-public Inventory (Items[] Items) 
+public Inventory (Item[] Items) 
 {
     this.Items = Items;
 }
@@ -21,37 +23,3 @@ public void AddItem (Item NewItem) {
         } 
     }
 }
-   
-// Method for removing items from inventory
-public void RemoveItem (string ItemName) 
-{
-    for (int i = 0 ; i<Items.Length ; i++) 
-    {
-        if (Items[i] != null && Items[i].GetName() == ItemName)
-        {
-            Items[i]= null;
-            break; 
-        }
-    }
-}
-*/
-
-using System.Collections;
-using System.Security.Cryptography;
-
-
-
-
-/*class Item
-{
-    public string title;
-    public int effect;
-    public int keyNumber;
-    public Item(string _title, int _effect, int _keyNumber)
-    {
-        this.title = _title;
-        this.effect = _effect;
-        this.keyNumber = _keyNumber;
-    }
-}
-*/ 

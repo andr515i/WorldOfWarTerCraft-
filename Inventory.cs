6c7a0421326323c19cec240df79772e869d225b9
L-tech-s <joachimlow02@gmail.com>
2024-11-04 11:57:28 +0100

    Update Inventory layout.cs
    Dashes and names for each parameter

--- Inventory.cs
+++ Inventory.cs
@@ -3,26 +3,35 @@ using System.Security.Cryptography;
 class Inventory 
 {
 // Atributes 
-    private Item[] Items;
+private Item[] Items;
 // Constructor 
 public Inventory (Item[] Items) 
 {
     this.Items = Items;
 }
 
-// Inventory display
-public void InventoryDisplay() {
-    Console.WriteLine("Inventory:"); 
-    for (int i = 0 ; i< Items.Length ; i++ )
+static string PrintDashes(int length)
     {
- //Makes sure it only prints Item in inventory if there are any.
-            if (Items[i]!= null) {
-           
-            Console.WriteLine(Items[i]);
-            }
-        
+        return new string('-', length);
     }
-} 
+
+// Inventory display
+public void InventoryDisplay()
+{
+    Console.WriteLine("Inventory:");
+    Console.WriteLine("Name \t\t Effect \t Description");
+    Console.WriteLine(PrintDashes(44));
+        
+    for (int i = 0; i < Items.Length; i++)
+    {
+        // Makes sure it only prints Item in inventory if there are any.
+        if (Items[i] != null)
+        {
+            Console.WriteLine(Items[i]);
+        }
+    }
+}
+
 // Method for adding Items to inventory
 public void AddItem (Item NewItem) 
 {
